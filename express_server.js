0f9c6943ec5794585ed76fc4913b3d1ac023165f
Vaishali Gupta <grvaishali@gmail.com>
2020-09-07 23:02:26 -0400

    Changes done as per review

--- express_server.js
+++ express_server.js
@@ -7,7 +7,6 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = 8080;
 const salt = bcrypt.genSaltSync(10);
-const pwd = bcrypt.hashSync("pockpock", salt);
 
 const { generateRandomString, lookUp, checkCredentials, checkUsername, getUserId, urlsForUser } = require('./helpers.js');
 const { users, urlDatabase } = require('./database.js');
@@ -36,7 +35,7 @@ app.listen(PORT, () => {
 
 //HOME
 app.get("/", (req, res) => {
-  if (req.session.user_id) {
+  if (req.session.user_id !== undefined && users[req.session.user_id] !== undefined) {
     res.redirect('/urls');
   } else {
     res.redirect('/login');
@@ -45,12 +44,16 @@ app.get("/", (req, res) => {
 
 // REGISTER
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  if (req.session.user_id === undefined || users[req.session.user_id] === undefined) {
+    res.render("urls_register");
+  } else {
+    res.redirect('/urls');
+  }
 });
 
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
-  if (email == "" || password == "") {
+  if (email === "" || password === "") {
     res.status(400).send('Email and password can not be empty');
   }
   if (lookUp(email, password, users) === 400) {
@@ -71,6 +74,9 @@ app.get("/urls", (req, res) => {
   if (req.session.user_id !== undefined && users[req.session.user_id] !== undefined) {
     email = users[req.session.user_id].email;
     currentUser = true;
+  } else {
+    res.send("Access denied");
+    return;
   }
   let templateVars = { urls: urlsForUser(req.session.user_id, urlDatabase), email: email, currentUser: currentUser };
   res.render("urls_index", templateVars);
@@ -88,6 +94,7 @@ app.get("/urls/new", (req, res) => {
     currentUser = true;
   } else {
     res.redirect('/login');
+    return;
   }
   let templateVars = { email: email, currentUser: currentUser };
   res.render("urls_new", templateVars);
@@ -100,6 +107,17 @@ app.get("/urls/:shortURL", (req, res) => {
   if (req.session.user_id !== undefined && users[req.session.user_id] !== undefined) {
     email = users[req.session.user_id].email;
     currentUser = true;
+  } else {
+    res.send("Access denied");
+    return;
+  }
+  if (urlDatabase[req.params.shortURL] === undefined) {
+    res.send("URL doesn't exist");
+    return;
+  }
+  if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
+    res.send("URL belongs to some other user. Access denied.");
+    return;
   }
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, email: email, currentUser: currentUser };
   res.render("urls_show", templateVars);
@@ -115,6 +133,10 @@ app.post("/urls", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const url = urlDatabase[req.params.shortURL];
+  if (url === undefined) {
+    res.send("URL doesn't exist");
+    return;
+  }
   res.redirect(url.longURL);
 });
 
@@ -124,6 +146,7 @@ app.put('/urls/:shortURL', (req, res) => {
   if (userURLs[req.params.shortURL]) {
     urlDatabase[req.params.shortURL].longURL = req.body.longURL;
     res.redirect(`/urls/${req.params.shortURL}`);
+    return;
   }
 
   res.status(400);
@@ -135,6 +158,7 @@ app.delete('/urls/:shortURL', (req, res) => {
   if (userURLs[req.params.shortURL]) {
     delete urlDatabase[req.params.shortURL];
     res.redirect('/urls');
+    return;
   }
 
   res.status(400);
@@ -143,12 +167,21 @@ app.delete('/urls/:shortURL', (req, res) => {
 
 //LOGIN
 app.get('/login', (req, res) => {
-  res.render('urls_login');
+  if (req.session.user_id === undefined || users[req.session.user_id] === undefined) {
+    res.render('urls_login');
+  } else {
+    res.redirect('/urls');
+  }
 });
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, salt);
+
+  if (email === "" || password === "") {
+    res.send("Please enter all the details");
+    return;
+  }
   if (checkUsername(email, users) === 200) {
     if (checkCredentials(email, hashedPassword, users) === 200) {
       req.session.user_id = getUserId(email, users);
@@ -157,6 +190,7 @@ app.post('/login', (req, res) => {
       res.status(403);
       res.send('Password incorrect');
     }
+    return;
   } else {
     res.status(403);
     res.send('No user with that email address exists');
